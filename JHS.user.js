1d97c99a182f4b7883e1bf4d861846651f5d12b7
Teamper <88786089+Teamper@users.noreply.github.com>
2026-05-28 11:10:57 +0800

    Fix: 修复快速筛选无法显示已标记项目的问题

--- JHS.user.js
+++ JHS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JHS-YA
 // @namespace    https://sleazyfork.org/zh-CN/scripts/578503-jhs-ya
-// @version      3.7.4
+// @version      3.7.5
 // @author       yaoser
 // @description  Jav-鉴黄师个人维护版：收藏、屏蔽、标记已下载、演员黑名单、收藏演员同步、新作品检测、热播/Top250/Fc2ppv/评论增强、相关清单、WebDAV数据备份、以图识图、字幕搜索；支持 JavDB / JavBus。
 // @license      MIT
@@ -4747,7 +4747,9 @@ class Ie extends X {
     applyVisibility() {
         const e = this.activeQuickFilter || "waitCheck", t = this.getSelector().itemSelector;
         $(t).each((function() {
-            const n = $(this), a = n.attr("data-hide") === "yes", i = n.attr("data-jhs-status") || "waitCheck", s = "all" !== e && i !== e;
+            const n = $(this), a = n.attr("data-hide") === "yes", i = n.attr("data-jhs-status") || "waitCheck";
+            if (e !== "all" && i === e) { n.show(); return; }
+            const s = "all" !== e && i !== e;
             a || s ? n.hide() : n.show();
         }));
     }
